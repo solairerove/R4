7b7ca1c383ddfa5162a69df9cd8a94829480259a
solairerove <hax2033@gmail.com>
2017-03-10 16:06:35 +0300

    Release candidate 1

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,10 +1,22 @@
 package sample;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -13,12 +25,29 @@ import java.util.*;
 
 public class Main extends Application {
 
+    final static Map<String, Double> map = new HashMap<>();
+    final static String itemA = "A";
+    final static String itemB = "B";
+    final static String itemC = "F";
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public void start(Stage stage) throws Exception{
+        final NumberAxis xAxis = new NumberAxis();
+        final CategoryAxis yAxis = new CategoryAxis();
+        final BarChart<Number, String> bc = new BarChart<>(xAxis, yAxis);
+        bc.setTitle("Summary");
+        xAxis.setLabel("Value");
+        xAxis.setTickLabelRotation(90);
+        yAxis.setLabel("Item");
+
+        XYChart.Series series1 = new XYChart.Series();
+        series1.setName("Metric");
+        for(String i : map.keySet()){
+            series1.getData().add(new XYChart.Data(map.get(i), i));
+        }
+        Scene scene = new Scene(bc, 800, 600);
+        bc.getData().addAll(series1);
+        stage.setScene(scene);
+        stage.show();
     }
 
 
@@ -54,6 +83,7 @@ public class Main extends Application {
                 sb.append(variantnessList.getName()).append(" ");
                 Map<String, Double> x = new HashMap<>();
                 x.put(variantnessList.getName(), value);
+                map.put(variantnessList.getName(), value);
                 output.add(x);
                 sb.append("\n");
             }
@@ -62,7 +92,7 @@ public class Main extends Application {
         /*CreateHtml(output);
         Drawer.addTail(js);
         Drawer.CreateFile(js.ToString());*/
-        Files.write(Paths.get("out.txt"), String.valueOf(sb).getBytes());
+        Files.write(Paths.get("outtest.txt"), String.valueOf(sb).getBytes());
         launch(args);
     }
 }
